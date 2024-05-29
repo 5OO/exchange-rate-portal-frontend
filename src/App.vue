d756291cd4d6c15a53716790ba84a2d80c1c1690
5OO <git.hub@myrk.eu>
2024-05-29 16:22:55 +0300

    Remove HelloWorld, TheWelcome, and WelcomeItem components
    The components HelloWorld, TheWelcome, and WelcomeItem were deleted. The imports in HomeView and App components were also removed.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -36,15 +35,7 @@ nav {
   text-align: center;
   margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
+ 
 nav a {
   display: inline-block;
   padding: 0 1rem;
