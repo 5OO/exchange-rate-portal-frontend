b328f8bbea3b4a443f021dc46994c68c43250277
5OO <git.hub@myrk.eu>
2024-06-01 14:11:47 +0300

    Add exchange rates history feature
    Implemented a new feature to display the exchange rates history for a specific currency on `ExchangeRateHistoryView`. An API call is made to fetch the exchange rates history during the component's mounting process, and these data is then displayed on a table for a clear overview. Moreover, the layout of `ExchangeRateHistoryView` has been updated accordingly to accommodate these changes.

--- src/views/ExchangeRateHistoryView.vue
+++ src/views/ExchangeRateHistoryView.vue
@@ -1,7 +1,59 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const currency = route.params.currency;
+const exchangeRates = ref([]);
+const currencyName = ref('');
+const currencyLocation = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`http://localhost:8080/api/exchange-rates/history/${currency}`);
+    exchangeRates.value = response.data;
+
+    if (exchangeRates.value.length > 0) {
+      currencyName.value = exchangeRates.value[0].currencyName;
+      currencyLocation.value = exchangeRates.value[0].entityLocation;
+    }
+  } catch (error) {
+    console.error("Error fetching exchange rates history:", error);
+  }
+});
+</script>
+
 <template>
-  <div class="history">
-    <h1>This is an about page</h1>
-  </div>
+  <main class="container mt-5">
+    <h1 class="mb-4">Exchange Rate History for {{ currency }}</h1>
+    <p>The exchange rates of the euro against foreign currencies of the European Central Bank (included in database since 30/09/2014) are presented according to their announcement date.</p>
+    <div v-if="exchangeRates.length">
+      <p>Currency name: {{ currencyName }}</p>
+      <p>Currency location: {{ currencyLocation }}</p>
+      <table class="table table-striped">
+        <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Currency</th>
+          <th scope="col">Rate</th>
+          <th scope="col">Currency Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="rate in exchangeRates" :key="rate.date">
+          <td>{{ rate.date }}</td>
+          <td>{{ rate.currency }}</td>
+          <td>{{ rate.rate }}</td>
+          <td>{{ rate.currencyName }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <p>Loading exchange rates history...</p>
+    </div>
+  </main>
 </template>
 
 <style>
