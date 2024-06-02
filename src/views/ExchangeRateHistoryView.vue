34591d7f350f20f6d59ed5f5904470063dbd4f83
5OO <git.hub@myrk.eu>
2024-06-02 22:34:39 +0300

    Implement loading and error handling in ExchangeRateHistoryView
    Added 'loading' and 'error' references to manage the state of fetching exchange rates. Display appropriate messages on the screen according to the state. Replaced the console error message with an error message display on the user interface.

--- src/views/ExchangeRateHistoryView.vue
+++ src/views/ExchangeRateHistoryView.vue
@@ -11,6 +11,8 @@ const currencyLocation = ref('')
 const currentPage = ref(0)
 const pageSize = ref(250)
 const totalPages = ref(0)
+const loading = ref(true)
+const error = ref(null)
 
 const fetchRates = async (page, size) => {
   try {
@@ -29,10 +31,12 @@ const fetchRates = async (page, size) => {
       currencyName.value = exchangeRates.value[0].currencyName
       currencyLocation.value = exchangeRates.value[0].entityLocation
     }
-  } catch (error) {
-    console.error('Error fetching exchange rates history:', error)
+  } catch (err) {
+    error.value = err.response?.data?.message || 'An error occurred while fetching exchange rates history'
+  } finally {
+    loading.value = false;
   }
-}
+};
 
 onMounted(() => {
   fetchRates(currentPage.value, pageSize.value)
@@ -55,36 +59,44 @@ const formatRate = (rate) => {
     <h1 class="mb-4">Exchange Rate History for {{ currency }}</h1>
     <p>The exchange rates of the euro against foreign currencies of the European Central Bank (included in database
       since 30/09/2014) are presented according to their announcement date.</p>
-    <div v-if="exchangeRates.length">
-      <p>Currency name: {{ currencyName }}</p>
-      <p>Currency location: {{ currencyLocation }}</p>
-      <table class="table table-striped">
-        <thead>
-        <tr>
-          <th scope="col">Date</th>
-          <th scope="col">Currency</th>
-          <th scope="col">Rate</th>
-          <th scope="col">Currency Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="rate in exchangeRates" :key="rate.date">
-          <td>{{ rate.date }}</td>
-          <td>{{ rate.currency }}</td>
-          <td>{{ formatRate(rate.rate) }}</td>
-          <td>{{ rate.currencyName }}</td>
-        </tr>
-        </tbody>
-      </table>
-      <div class="pagination">
-        <button @click="goToPage(currentPage.value - 1)" :disabled="currentPage.value === 0">Previous</button>
-        <span>Page {{ currentPage.value + 1 }} of {{ totalPages }}</span>
-        <button @click="goToPage(currentPage.value + 1)" :disabled="currentPage.value === totalPages.value - 1">Next
-        </button>
-      </div>
+    <div v-if="loading">
+      <p>Loading exchange rates history...</p>
     </div>
     <div v-else>
-      <p>Loading exchange rates history...</p>
+      <div v-if="error">
+        <p>{{ error }}</p>
+      </div>
+      <div v-else-if="exchangeRates.length">
+        <p>Currency name: {{ currencyName }}</p>
+        <p>Currency location: {{ currencyLocation }}</p>
+        <table class="table table-striped">
+          <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Currency</th>
+            <th scope="col">Rate</th>
+            <th scope="col">Currency Name</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="rate in exchangeRates" :key="rate.date">
+            <td>{{ rate.date }}</td>
+            <td>{{ rate.currency }}</td>
+            <td>{{ formatRate(rate.rate) }}</td>
+            <td>{{ rate.currencyName }}</td>
+          </tr>
+          </tbody>
+        </table>
+        <div class="pagination">
+          <button @click="goToPage(currentPage.value - 1)" :disabled="currentPage.value === 0">Previous</button>
+          <span>Page {{ currentPage.value + 1 }} of {{ totalPages }}</span>
+          <button @click="goToPage(currentPage.value + 1)" :disabled="currentPage.value === totalPages.value - 1">Next
+          </button>
+        </div>
+      </div>
+      <div v-else>
+        <p>No exchange rate history found.</p>
+      </div>
     </div>
   </main>
 </template>
